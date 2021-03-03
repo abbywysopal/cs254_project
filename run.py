901e9a5d460e798a58b7f93670d45a2d98d4d5ef
Abby Wysopal <abbywysopal@ucsb.edu>
2021-03-03 15:19:11 -0800

    generates blasic blocks, runs bb through cpusim, outputs a json with all lowercase letters and no puncutation and cycle count

--- run.py
+++ run.py
@@ -1,13 +1,24 @@
 import os
 
-os.system('python cpusim_generator.py tests/data/test/ 200')
-os.system('python cpusim_generator.py tests/data/train/ 2000')
-os.system('python cpusim_generator.py tests/data/valid/ 2000')
+os.system('rm -f -R cpusim/tests/data/')
+os.system('mkdir cpusim/tests/data/')
+os.system('mkdir cpusim/tests/data/test')
+os.system('mkdir cpusim/tests/data/test/json')
 
-os.system('python cpusim.py tests/data/test/')
-os.system('python cpusim.py tests/data/train/')
-os.system('python cpusim.py tests/data/valid/')
+# os.system('mkdir cpusim/tests/data/train')
+# os.system('mkdir cpusim/tests/data/valid')
 
-os.system('python create_json.py tests/data/test/')
-os.system('python create_json.py tests/data/train/')
-os.system('python create_json.py tests/data/valid/')
+# os.system('mkdir cpusim/tests/data/train/json')
+# os.system('mkdir cpusim/tests/data/valid/json')
+
+os.system('python cpusim/cpusim_generate_basic_block.py cpusim/tests/data/test/ 10')
+# os.system('python cpusim/cpusim_generator.py cpusim/tests/data/train/ 2000')
+# os.system('python cpusim/cpusim_generator.py cpusim/tests/data/valid/ 200')
+
+os.system('python cpusim/cpusim.py cpusim/tests/data/test/')
+# os.system('python cpusim/cpusim.py cpusim/tests/data/train/')
+# os.system('python cpusim/cpusim.py cpusim/tests/data/valid/')
+
+os.system('python cpusim/create_json.py cpusim/tests/data/test/')
+# os.system('python cpusim/create_json.py cpusim/tests/data/train/')
+# os.system('python cpusim/create_json.py cpusim/tests/data/valid/')
