af12bf5434c1d45f5139ed026f70ef912af873c7
alexrasla <alexrasla@ucsb.edu>
2021-03-04 11:22:44 -0800

    parameter changes

--- model.py
+++ model.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
-from tensorflow_core.python.keras.utils.data_utils import Sequence
+# from tensorflow_core.python.keras.utils.data_utils import Sequence
 from tensorflow.keras.utils import Sequence
 
 import json
@@ -17,11 +17,11 @@ import numpy as np
 # ]
 
 vocab_size = 10000
-embedding_dim = 16
-max_length = 4
+embedding_dim = 64
+max_length = 10
 trunc_type='post'
 padding_type='post'
-TRAINING_SIZE = 2
+TRAINING_SIZE = 4
 
 with open('tests/boolexpr/boolexpr.ass_json.txt', 'r') as f:
     data = json.load(f)
@@ -53,7 +53,7 @@ training_padded = pad_sequences(training_sequences, maxlen=max_length, padding=p
 testing_sequences = tokenizer.texts_to_sequences(testing_instructions)
 testing_padded = pad_sequences(testing_sequences, maxlen=max_length, padding=padding_type)
 
-print(training_padded.shape)
+# print(training_padded.shape)
 # print(training_sequences.shape)
 
 training_padded = np.array(training_padded)
@@ -67,20 +67,20 @@ testing_labels = np.array(testing_labels)
 
 model = tf.keras.Sequential([
     tf.keras.layers.Embedding(vocab_size, embedding_dim, input_length=max_length),
-    tf.keras.layers.LSTM(64), #input_shape=(1,1,4)
+    tf.keras.layers.LSTM(64, return_sequences=True), #input_shape=(1,1,4)
+    tf.keras.layers.LSTM(64),
+    # tf.keras.layers.Linear(64), 
     tf.keras.layers.Dense(64, activation='relu'),
     tf.keras.layers.Dense(1, activation='sigmoid')
 ])
 
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-# model.build()
-# model.summary()
+model.summary()
 
-epochs = 5
-
-# print(training_sequences)
+epochs = 50
 
 history = model.fit(training_padded, training_labels, 
 epochs=epochs, validation_data=(testing_padded, testing_labels), verbose=2)
     
 # print(history)
+print(model.predict(testing_padded))
